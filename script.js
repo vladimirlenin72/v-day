78de5c6fe9563846408d6f17eb194176282ab5ce
vladimirlenin72 <danielkristo25@gmail.com>
2026-03-16 23:40:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const gifStages = [
     "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
     "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
+    "https://c.tenor.com/GkjKfwjeb-EAAAAC/tenor.gif",             // 2 pleading
     "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
+    "music/Puppy Eyes.jpeg",       // 4 sadder
     "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
     "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
     "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
@@ -22,6 +22,7 @@ const noMessages = [
 ]
 
 const yesTeasePokes = [
+    "Ouch, we forgot. Make a wish first, Honey!",
     "Just try saying no first. I bet you want to know what happens.",
     "Go on, hit no, Honey. Just once.",
     "You're missing out...",
